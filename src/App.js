c2ba97b5147111d3770ba47a9ba938d3000a710a
Hassanamiri <hassanamiri369@gmail.com>
2023-03-10 21:50:05 +0330

    added Home route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import {Routes , Route , Link} from "react-router-dom"
+
+
+// page
+import Home from './components/Home';
+
 
 const App = () => {
   return (
     <div>
         <h1>React Redux CRUD APP Material ui - GET POST PUT DELETE</h1>
+
+
+      <div className="Routes-content">
+        <Routes>
+            <Route path='/' element={<Home/>}/>
+        </Routes>
+      </div>
     </div>
   )
 }
