61065e4aa9d70c047375fff3b7ceee86cae25a5c
Hassanamiri <hassanamiri369@gmail.com>
2023-03-11 07:31:42 +0330

    create delete method

--- src/Redux/action.js
+++ src/Redux/action.js
@@ -2,16 +2,28 @@ import axios from "axios";
 import * as types from "./actionType";
 
 
-const getUSers = (users) =>({type : types.GET_USERS , payload : users})
-
+const getUSER = (users) =>({type : types.GET_USERS , payload : users})
+const deleteUSER = () => ({type : types.DELETE_USER})
 
 export const loadUsers = ()=>{
     return (dispatch)=>{
         axios.get("http://localhost:3004/users")
         .then((res)=> {
             console.log("res" , res);
-            dispatch(getUSers(res.data))
+            dispatch(getUSER(res.data))
         })
         .catch((error)=> console.log(error))
     }
 };
+
+
+export const deleteUser = (id) =>{
+    return(dispatch) =>{
+        axios.delete(`http://localhost:3004/users/${id}`)
+        .then((res) => {
+            console.log('res' , res);
+            dispatch(deleteUSER())
+            dispatch(loadUsers())
+        })
+    }
+}
