bc136fb685127034aaec74b6f3697ad877b714ae
Hassanamiri <hassanamiri369@gmail.com>
2023-03-11 10:21:52 +0330

    create addUser action

--- src/Redux/action.js
+++ src/Redux/action.js
@@ -4,6 +4,7 @@ import * as types from "./actionType";
 
 const getUSER = (users) =>({type : types.GET_USERS , payload : users})
 const deleteUSER = () => ({type : types.DELETE_USER})
+const addUSER = () =>({type : types.ADD_USER})
 
 export const loadUsers = ()=>{
     return (dispatch)=>{
@@ -25,5 +26,18 @@ export const deleteUser = (id) =>{
             dispatch(deleteUSER())
             dispatch(loadUsers())
         })
+    }
+}
+
+
+export const addUser = (user)=>{
+    return (dispatch)=>{
+        axios.post("http://localhost:3004/users" , user)
+        .then((res) => {
+            console.log("res" , res);
+            dispatch(addUSER())
+            dispatch(loadUsers())
+        })
+        .catch((error) => console.log(error))
     }
 }
