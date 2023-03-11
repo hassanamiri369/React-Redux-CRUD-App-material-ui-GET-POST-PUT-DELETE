c51ec855b1f9974edfebc50350bef93fba89ec32
Hassanamiri <hassanamiri369@gmail.com>
2023-03-11 18:22:38 +0330

    added table and fix style home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,24 @@
-import React, { useEffect , useState} from 'react'
+import React, { useEffect } from 'react'
 
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteUser, loadUsers } from '../Redux/action';
 import { Link } from 'react-router-dom';
 
 
+// style
+import { Button } from '@mui/material';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+
 const Home = () => {
     const dispatch = useDispatch();
-    const { users, user, loading } = useSelector((state) => state.users)
+    const { users, loading } = useSelector((state) => state.users)
 
 
     useEffect(() => {
@@ -20,26 +31,61 @@ const Home = () => {
         if (window.confirm("are you sure wanted to delete the user ? ")) {
             dispatch(deleteUser(id))
         }
-
-
     }
-
-    // if(loading){
-    //     return <h2>Loading ...</h2>
-    // }
-
 
 
 
     return (
 
-        <>
-            <div className='home-header'>
-                <div>
-                    <Link to={'/addUser'}>Add User</Link>
-                </div>
+        <div style={{display : "flex" , justifyContent : "center" , flexDirection : "column" , alignItems : "center"}}>
+
+        <div>
+            <h1>React Redux CRUD App</h1>
+        </div>
+
+            <div style={{width : "90%" , minHeight : "100vh" , marginBottom : "100px"}}>
+
+                   
+                        <div style={{height : "6rem"}}>
+                            <Link to={'/addUser'}><Button variant='contained' color='success'>Add User</Button></Link>
+                        </div>
+                
+
+                <TableContainer>
+                    <Table sx={{ minWidth: 650 }} >
+
+                        <TableHead>
+                            <TableRow selected>
+                                <TableCell>ID</TableCell>
+                                <TableCell>Name</TableCell>
+                                <TableCell>Email</TableCell>
+                                <TableCell>Contact</TableCell>
+                                <TableCell>Address</TableCell>
+                                <TableCell>Action</TableCell>
+                            </TableRow>
+                        </TableHead>
+
+
+                        <TableBody>
+                            {users && users.map((item, index) => (
+                                <TableRow  key={item.id}>
+                                    <TableCell >{item.id}</TableCell>
+                                    <TableCell >{item.name}</TableCell>
+                                    <TableCell >{item.email}</TableCell>
+                                    <TableCell >{item.contact}</TableCell>
+                                    <TableCell >{item.address}</TableCell>
+                                    <TableCell >
+                                        <Button style={{margin : "3px"}} variant="contained" color="secondary" onClick={() => handleDelete(item.id)}>delete</Button>
+                                        <Link  to={`/editUser/${item.id}`}><Button style={{margin : "3px"}} variant='contained'>Edit</Button></Link>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
             </div>
-            <div className='home-container'>
+           
+            {/* <div className='home-container'>
                 {loading && <h1>Loading data...</h1>}
 
                 {users && users.map((item, index) => (
@@ -54,8 +100,8 @@ const Home = () => {
                         </span>
                     </div>
                 ))}
-            </div>
-        </>
+            </div> */}
+        </div>
     )
 }
 
