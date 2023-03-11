5784e5dc1e86f980cdd642ad6b0820c0f5852fa7
Hassanamiri <hassanamiri369@gmail.com>
2023-03-11 18:22:55 +0330

    added alert notification

--- src/pages/EditUser.js
+++ src/pages/EditUser.js
@@ -1,7 +1,8 @@
 import React , {useState , useEffect} from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Outlet, useNavigate, useParams } from 'react-router-dom'
-import { addUser, getSingleUser, updateUser } from '../Redux/action'
+import {getSingleUser, updateUser } from '../Redux/action'
+import { Alert, Button, TextField } from '@mui/material';
 
 const EditUser = () => {
 
@@ -61,32 +62,35 @@ const EditUser = () => {
     return (
         <div>
             <Outlet/>
-             <div>
-                <h1>Edit USER</h1>
-                <p>{error && <span>{error}</span>}</p>
-            </div>
-           <div className='add-form'>
-            <p>{id}</p>
-               <form autoComplete='off' noValidate onSubmit={handleSubmit}>
-               <div>
-                    <input onChange={handleInputChange} placeholder='name' type="text" name='name' value={name || ""} />
-                </div>
-                <div>
-                    <input onChange={handleInputChange} placeholder='email' type="text" name='email' value={email || ""} />
-                </div>
-                <div>
-                    <input onChange={handleInputChange} placeholder='contact' type="text" name='contact' value={contact || ""} />
-                </div>
-                <div>
-                    <input onChange={handleInputChange} placeholder='address' type="text" name='address' value={address || ""} />
-                </div>
-                <div>
-                    <button type='submit'>edit user</button>
-                    <button onClick={()=>navigate("/")}>cancel</button>
-                </div>
-               </form>
-           </div>
+        <div className='head'>
+            <h1>EDIT USER</h1>
+             <div style={{backgroundColor : "blue" , width : "70%" , borderRadius : "5px"}}>{error && <Alert severity='error' >{error}</Alert>}</div>
         </div>
+
+
+        <div className='add-form'>
+            <form autoComplete='off' noValidate onSubmit={handleSubmit}>
+                <div>
+                    <TextField  required placeholder='Ali' onChange={handleInputChange} label="Name" variant='outlined' type='text' name='name' value={name || ""} />
+                </div>
+                <div>
+                    <TextField  required placeholder='hassanamiri369@gmail.com' onChange={handleInputChange} label="Email" variant='outlined' type='email' name='email' value={email || ""} />
+                </div>
+                <div>
+                    <TextField required placeholder='09109705963' onChange={handleInputChange} label="Contact" variant='outlined' type='text' name='contact' value={contact || ""} />
+
+                </div>
+                <div>
+                    <TextField required placeholder='tehran ,Navab ,street 23' onChange={handleInputChange} label="Address" variant='outlined' type='text' name='address' value={address || ""} />
+                </div>
+                <div className='buttons-form'>
+                    <Button color='primary' type='submit' variant='contained'>Edit User</Button>
+                    
+                    <Button color='secondary' variant='contained' onClick={() => navigate("/")}>cancel</Button>
+                </div>
+            </form>
+        </div>
+    </div>
     )
 }
 
