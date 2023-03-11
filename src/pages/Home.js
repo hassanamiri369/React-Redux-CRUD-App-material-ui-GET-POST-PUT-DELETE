e0f2737f419fc64adbd335342bf43a965002024c
Hassanamiri <hassanamiri369@gmail.com>
2023-03-11 07:41:15 +0330

    handle get and delete and show data in view

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,23 @@
-import React , {useEffect } from 'react'
+import React, { useEffect } from 'react'
 
-import { useDispatch , useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { deleteUser, loadUsers } from '../Redux/action';
+import { Link } from 'react-router-dom';
 
 
 const Home = () => {
     const dispatch = useDispatch();
-    const {users , user , loading} = useSelector((state) => state.users)
+    const { users, user, loading } = useSelector((state) => state.users)
 
 
-    useEffect(()=>{
+    useEffect(() => {
         dispatch(loadUsers())
-    } , [])
+    }, [])
 
 
 
-    const handleDelete = (id) =>{
-        if(window.confirm("are you sure wanted to delete the user ? ")){
+    const handleDelete = (id) => {
+        if (window.confirm("are you sure wanted to delete the user ? ")) {
             dispatch(deleteUser(id))
         }
 
@@ -25,20 +26,28 @@ const Home = () => {
 
 
     return (
-        <div className='home-container'>
-            {users && users.map((item , index) => (
-                <div key={item.id}>
-                    <span>{item.name}</span>
-                    <span>{item.email}</span>
-                    <span>{item.contact}</span>
-                    <span>{item.address}</span>
-                    <span>
-                        <button onClick={()=>handleDelete(item.id)}>delete</button>
-                        <button>edit</button>
-                    </span>
+
+        <>
+            <div className='home-header'>
+                <div>
+                    <Link to={'/addUser'}>Add User</Link>
                 </div>
-            ))}
-        </div>
+            </div>
+            <div className='home-container'>
+                {users && users.map((item, index) => (
+                    <div key={item.id}>
+                        <span>{item.name}</span>
+                        <span>{item.email}</span>
+                        <span>{item.contact}</span>
+                        <span>{item.address}</span>
+                        <span>
+                            <button onClick={() => handleDelete(item.id)}>delete</button>
+                            <button>edit</button>
+                        </span>
+                    </div>
+                ))}
+            </div>
+        </>
     )
 }
 
