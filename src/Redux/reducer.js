f6a03c6e0efa491bb9f1bd0a23f77ddad8b9ad53
Hassanamiri <hassanamiri369@gmail.com>
2023-03-11 07:32:30 +0330

    get and delete action type for reducer and update state

--- src/Redux/reducer.js
+++ src/Redux/reducer.js
@@ -1,12 +1,20 @@
+import * as types from "./actionType";
+
+
 const initState = {
     users : [],
     user : {},
-    loading : false,
+    loading : true,
 }
 
 
 const userReducer = (state = initState , action) =>{
     switch(action.type){
+        case types.GET_USERS : 
+            return {...state , users : action.payload , loading : false}
+
+        case types.DELETE_USER : 
+            return {...state , loading : false}
         default : 
           return state
     }
