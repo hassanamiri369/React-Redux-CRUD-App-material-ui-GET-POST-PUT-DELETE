f5e63f4cfe4a9e1c9da5c45ecb188b8e1c09d84c
Hassanamiri <hassanamiri369@gmail.com>
2023-03-11 16:57:25 +0330

    fix addUser page style

--- src/pages/AddUser.js
+++ src/pages/AddUser.js
@@ -1,41 +1,43 @@
-import React , {useState , useEffect}from 'react'
+import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { addUser } from '../Redux/action';
+import { Alert, Box, Button, TextField } from '@mui/material';
 
+// style
 
 const AddUser = () => {
 
     const navigate = useNavigate()
     const dispatch = useDispatch()
-    
-    const [state , setState] = useState({
-        name : "",
-        email : "",
-        contact : "",
-        address : ""
+
+    const [state, setState] = useState({
+        name: "",
+        email: "",
+        contact: "",
+        address: ""
     })
 
-    const {name , email , contact , address} = state;
+    const { name, email, contact, address } = state;
 
-    const [error , setError] = useState("");
-
-
+    const [error, setError] = useState("");
 
 
 
-    const handleInputChange = (e)=>{
-        let {name , value} = e.target;
-        setState({...state , [name] : value})
+
+
+    const handleInputChange = (e) => {
+        let { name, value } = e.target;
+        setState({ ...state, [name]: value })
     }
 
 
-    const handleSubmit = (e)=>{
+    const handleSubmit = (e) => {
         e.preventDefault()
 
-        if(!name || !email || !address || !contact){
+        if (!name || !email || !address || !contact) {
             setError("Please input all input field")
-        }else{
+        } else {
             dispatch(addUser(state))
             navigate("/")
             setError("")
@@ -45,31 +47,35 @@ const AddUser = () => {
 
     return (
         <div className='addUser-container'>
-           
-            <div>
+
+            <div className='head'>
                 <h1>ADD USER</h1>
-                <p>{error && <span>{error}</span>}</p>
+                 <Alert severity='error' style={{textAlign : "center" , width : "70%"}}>{error && error}</Alert>
             </div>
-           <div className='add-form'>
-               <form autoComplete='off' noValidate onSubmit={handleSubmit}>
-               <div>
-                    <input onChange={handleInputChange} placeholder='name' type="text" name='name' value={name} />
-                </div>
-                <div>
-                    <input onChange={handleInputChange} placeholder='email' type="text" name='email' value={email} />
-                </div>
-                <div>
-                    <input onChange={handleInputChange} placeholder='contact' type="text" name='contact' value={contact} />
-                </div>
-                <div>
-                    <input onChange={handleInputChange} placeholder='address' type="text" name='address' value={address} />
-                </div>
-                <div>
-                    <button type='submit'>add user</button>
-                    <button onClick={()=>navigate("/")}>cancel</button>
-                </div>
-               </form>
-           </div>
+
+
+            <div className='add-form'>
+                <form autoComplete='off' noValidate onSubmit={handleSubmit}>
+                    <div>
+                        <TextField  required placeholder='Ali' onChange={handleInputChange} label="Name" variant='outlined' type='text' name='name' value={name} />
+                    </div>
+                    <div>
+                        <TextField  required placeholder='hassanamiri369@gmail.com' onChange={handleInputChange} label="Email" variant='outlined' type='email' name='email' value={email} />
+                    </div>
+                    <div>
+                        <TextField required placeholder='09109705963' onChange={handleInputChange} label="Contact" variant='outlined' type='number' name='contact' value={contact} />
+
+                    </div>
+                    <div>
+                        <TextField required placeholder='tehran ,Navab ,street 23' onChange={handleInputChange} label="Address" variant='outlined' type='text' name='address' value={address} />
+                    </div>
+                    <div className='buttons-form'>
+                        <Button color='primary' type='submit' variant='contained'>Add User</Button>
+                        
+                        <Button color='secondary' variant='contained' onClick={() => navigate("/")}>cancel</Button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
